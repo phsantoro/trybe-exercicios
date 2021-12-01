c36d45eb66b5ab38853e588911bc437a080f46ba
Philippe Santoro <phsantoro95@gmail.com>
2021-12-01 20:11:28 -0300

    Resoluçao exercicio 4

--- 1-Fundamentos/Bloco04-JavaScript/4.1-Primeiros-Passos/exercicio.js
+++ 1-Fundamentos/Bloco04-JavaScript/4.1-Primeiros-Passos/exercicio.js
@@ -41,3 +41,15 @@ if ( valueOne > valueTwo && valueOne > valueThree) {
     console.log("O maior número é: ", valueThree)
 }
 
+
+// 4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const number = -50
+
+if (number > 0) {
+    console.log("positive")
+} else if (number < 0 ) {
+    console.log("negative")
+} else {
+    console.log("zero")
+}
