54f716cf52c95e65da2019009ed1a7281bd0ed72
Philippe Santoro <phsantoro95@gmail.com>
2021-12-01 21:01:06 -0300

    Resoluçao exercicis 6

--- 1-Fundamentos/Bloco04-JavaScript/4.1-Primeiros-Passos/exercicio.js
+++ 1-Fundamentos/Bloco04-JavaScript/4.1-Primeiros-Passos/exercicio.js
@@ -9,22 +9,21 @@ let mult = a * b;
 let divide = a / b;
 let modulo = a % b;
 
-console.log(soma)
-console.log(subt)
-console.log(mult)
-console.log(divide)
-console.log(modulo)
-
+console.log(soma);
+console.log(subt);
+console.log(mult);
+console.log(divide);
+console.log(modulo);
 
 // 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
 const numberOne = 50;
 const numberTwo = 62;
 
-if (numberOne > numberTwo ) {
-    console.log("O maior número é: ", numberOne)
+if (numberOne > numberTwo) {
+  console.log("O maior número é: ", numberOne);
 } else {
-    console.log("O maior número é: ", numberTwo)
+  console.log("O maior número é: ", numberTwo);
 }
 
 // 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
@@ -33,25 +32,24 @@ const valueOne = 10;
 const valueTwo = 20;
 const valueThree = 30;
 
-if ( valueOne > valueTwo && valueOne > valueThree) {
-    console.log("O maior número é: ", valueOne)
+if (valueOne > valueTwo && valueOne > valueThree) {
+  console.log("O maior número é: ", valueOne);
 } else if (valueTwo > valueOne && valueTwo > valueThree) {
-    console.log("O maior número é: ", valueTwo)
+  console.log("O maior número é: ", valueTwo);
 } else {
-    console.log("O maior número é: ", valueThree)
+  console.log("O maior número é: ", valueThree);
 }
-
 
 // 4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
-const number = -50
+const number = -50;
 
 if (number > 0) {
-    console.log("positive")
-} else if (number < 0 ) {
-    console.log("negative")
+  console.log("positive");
+} else if (number < 0) {
+  console.log("negative");
 } else {
-    console.log("zero")
+  console.log("zero");
 }
 
 // 5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
@@ -60,11 +58,43 @@ const angulo1 = 50;
 const angulo2 = 60;
 const angulo3 = 70;
 
-let triangulo = angulo1 + angulo2 + angulo3
+let triangulo = angulo1 + angulo2 + angulo3;
 
-
-if (triangulo === 180 ) {
-    console.log(true)
+if (triangulo === 180) {
+  console.log(true);
 } else {
-    console.log(false)
+  console.log(false);
+}
+
+// 6. Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+
+let peça;
+
+switch (peça) {
+  case "Peão":
+    console.log("Anda uma casa para frente e come outra peça na diagonal");
+    break;
+
+  case "Torre":
+    console.log("Se movimenta para a horizontal e vertical");
+    break;
+
+  case "Cavalo":
+    console.log("Se movimenta em L");
+    break;
+
+  case "Bispo":
+    console.log("Se movimenta nas diagonais");
+    break;
+
+  case "Rainha":
+    console.log("Se movimenta na diagonal, vertical e horizontal");
+    break;
+
+  case "Rei":
+    console.log("Se movimenta na diagonal, vertical e horizontal, porém apenas uma casa");
+    break;
+
+    default:
+    console.log("Peça inválida")
 }
