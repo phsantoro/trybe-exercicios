6ba59220a2796a0d92cfc7197a2fd86a2236e6cf
Philippe Santoro <phsantoro95@gmail.com>
2021-12-15 17:11:59 -0300

    exemplo for in

---
+++ 1-Fundamentos/Bloco04-JavaScript/4.4-Objetos-funçoes/exemplo-for-in.js
@@ -0,0 +1,25 @@
+// 1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+  for ( let name in names) {
+      console.log('Olá', names[name]);
+  }
+
+  // 2 - Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+  let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let carro in car) {
+    console.log(carro + ': ' + car[carro])
+  }
+
+  
