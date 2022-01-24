11fdc5d9ea5b08d254393534b3fc0b11da972e40
Philippe Santoro <phsantoro95@gmail.com>
2022-01-24 15:07:29 -0300

    Exercicios 8.1

--- 1-Fundamentos/Bloco08-High-Order/8.1-Introdução-HOF/exemplo-fixação.js
+++ 1-Fundamentos/Bloco08-High-Order/8.1-Introdução-HOF/exemplo-fixação.js
@@ -1,13 +1,13 @@
- const acordar = () => 'Acordando!!';
+const acordar = () => "Acordando!!";
 
- const cafe = () => 'Bora tomar café!!';
+const cafe = () => "Bora tomar café!!";
 
- const dormir = () => 'Partiu dormir!!';
+const dormir = () => "Partiu dormir!!";
 
- const hof = (func) => {
-     const result = func();
-     console.log(result);
- };
+const hof = (func) => {
+  const result = func();
+  console.log(result);
+};
 
 hof(acordar);
 hof(cafe);
